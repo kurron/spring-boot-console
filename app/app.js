515c9300b71cd6fb2e63ae5b3ee611920bad6606
Ron Kurr <kurr@kurron.org>
2015-03-18 10:48:44 -0400

    Working on the connection information.

--- app/app.js
+++ app/app.js
@@ -1,8 +1,22 @@
 var app = angular.module('ConsoleApplication', ['ngMaterial']);
 
-app.controller('app', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-    $scope.toggleSidenav = function(menuId) {
-        $mdSidenav(menuId).toggle();
-    };
+app.controller('app', ['$scope', '$mdSidenav', function( $scope ){
+    $scope.connection = {
+        hostname: 'localhost',
+        port: 8080,
+        context: '/operations',
+        username: ' ',
+        password: ' ',
+        secure: true,
 
+        email: 'ipsum@lorem.com',
+        firstName: '',
+        lastName: '' ,
+        company: 'Google' ,
+        address: '1600 Amphitheatre Pkwy' ,
+        city: 'Mountain View' ,
+        state: 'CA' ,
+        biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+        postalCode : '94043'
+    };
 }]);
